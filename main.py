23be8b10444e5764b9e821e77b2a3f22c61f1bef
Srcy Dev <88575889+SrcyDev@users.noreply.github.com>
2022-06-14 16:40:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import hashlib
 from time import sleep
 import os
 
+os.system("")
+
 upper_letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 lower_letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 digits = ["0","1","2","3","4","5","6","7","8","9"]
